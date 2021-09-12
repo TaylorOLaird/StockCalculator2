f82c4719eea1ddee7c1277a261a7cddb143b401f
Taylor Laird <taylorolaird@knights.ucf.edu>
2021-09-12 00:48:00 -0400

    added the add stock method

--- StockCalculator2.java
+++ StockCalculator2.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
+import java.io.File;
 
 import java.text.*;
 
@@ -14,14 +15,21 @@ class Stock {
 	private float amountOwned;
 	private float quantityOwned;
 
+	private float yearlyAvgChange;
+	private float value;
+
 	public Stock() {
 		this.name = "default_Stock_name";
 		this.ticker = "default_Stock_ticker";
 		this.divYield = (float)0.0;
 		this.divFreqPerYear = 0;
 		this.compounding = false;
+
 		this.amountOwned = (float)0.0;
 		this.quantityOwned = (float)0.0;
+
+		this.yearlyAvgChange = (float)0.0;
+		this.value = (float)0.0;
 	}
 
 	public String toString() {
@@ -45,7 +53,7 @@ class Stock {
 		if (divYield <= 0)
 			System.out.println("invalid divYield, must be a positive float value");
 		else
-			this.ticker = ticker;
+			this.divYield = divYield;
 		return;
 	}
 
@@ -78,6 +86,19 @@ class Stock {
 		return;
 	}
 
+	public void setYearlyAvgChange(float yearlyAvgChange) {
+		this.yearlyAvgChange = yearlyAvgChange;
+		return;
+	}
+
+	public void setValue(float value) {
+		if (value <= 0)
+			System.out.println("invalid value, must be a positive float value");
+		else
+			this.value = value;
+		return;
+	}
+
 	public String getName() {
 		return this.name;
 	}
@@ -104,6 +125,14 @@ class Stock {
 
 	public float getQuantityOwned() {
 		return this.quantityOwned;
+	}
+
+	public float getYearlyAvgChange() {
+		return this.yearlyAvgChange;
+	}
+
+	public float getValue() {
+		return this.value;
 	}
 }
 
@@ -167,9 +196,12 @@ class SortByQuantityOwned implements Comparator<Stock> {
 class StockPortfolio {
 	private String name;
 	private ArrayList<Stock> portfolio;
+
 	public StockPortfolio (String name){
 		this.name = name;
 		this.portfolio = new ArrayList<Stock>();
+		System.out.println("name: " + this.name);
+		StockCalculator2.wait(1000);
 	}
 
 	// public String toString() {
@@ -178,6 +210,7 @@ class StockPortfolio {
 }
 
 public class StockCalculator2 {
+	private final int printWidth = 60;
 	// public static void printInvestment(float savings, float divRate, float addedSavings) {
 	// 	int totalTime = 50, printInterval = 1;
 	// 	for (int years = 0; years <= totalTime; years ++) {
@@ -194,13 +227,119 @@ public class StockCalculator2 {
 	// 	System.out.println("\n======================================================================================\n");
 	// }
 
-	private void uiLoop() {
+	// private StockPortfolio loadFile(String fileName) {
+		
+	// }
+
+	//TODO: fix bar printing should prob be a function
+
+	private void printUI(int amountOfBars, String text) {
+		for (int i = 0; i < amountOfBars; i++)
+			System.out.print("=");
+
+		System.out.println("\n\t" + text);
+
+		for (int i = 0; i < amountOfBars; i++)
+			System.out.print("=");
+
+		System.out.println();
+	}
+
+	private StockPortfolio createFile() {
 		Boolean uiRunning = true;
-		Boolean portfolioLoaded = false;
+		int waitTime = 1000;
+		String portfolioName = null;
+
 		while (uiRunning) {
 			clearScreen();
 			System.out.println("===========================================================");
-			System.out.println("\t1: Load file\n\t2: Save file\n\t3: Add stock");
+			System.out.println("\tEnter Portfolio name");
+			System.out.println("===========================================================");
+
+			Scanner sc = new Scanner(System.in);
+			portfolioName = sc.next();
+			portfolioName = portfolioName + ".txt";
+
+			File f = new File(portfolioName);
+
+			if (f.exists()) {
+				clearScreen();
+				System.out.println("===========================================================");
+				System.out.println("\tFile already exists");
+				System.out.println("\t0: Try new name\n\t1: Return");
+				System.out.println("===========================================================");
+
+				int choice = sc.nextInt();
+
+				if (choice == 1)
+					return null;
+				else if (choice != 1 && choice != 0) {
+					System.out.println("do invalid input stuff");
+					wait(waitTime);
+				}
+			} else {
+				uiRunning = false;
+			}
+		}
+
+		System.out.println("do create file stuff in method");
+		wait(waitTime);
+
+		return new StockPortfolio(portfolioName);
+	}
+
+	private StockPortfolio addStock(StockPortfolio portfolio) {
+		if (portfolio == null) {
+			System.out.println("technically return since portfolio is null");
+			wait(1000);
+		}
+		Stock tmpStock = new Stock();
+
+		clearScreen();
+		printUI(this.printWidth, "Enter Stock name");
+
+		Scanner sc = new Scanner(System.in);
+		tmpStock.setName(sc.nextLine());
+
+		printUI(this.printWidth, "Enter " + tmpStock.getName() + "'s current value");
+		tmpStock.setValue(sc.nextFloat());
+		sc.nextLine();
+
+		printUI(this.printWidth, "Enter " + tmpStock.getName() + "'s ticker");
+		tmpStock.setTicker(sc.nextLine());
+
+		printUI(this.printWidth, "Enter " + tmpStock.getName() + "'s divYield");
+		tmpStock.setDivYield(sc.nextFloat());
+
+		printUI(this.printWidth, "Enter " + tmpStock.getName() + "'s divFreqPerYear");
+		tmpStock.setDivFreqPerYear(sc.nextInt());
+
+		printUI(this.printWidth, "Enter if you are compounding " + tmpStock.getName() + "'s dividends, <true/false>");
+		tmpStock.setCompounding(sc.nextBoolean());
+
+		printUI(this.printWidth, "Enter amountOwned of" + tmpStock.getName());
+		tmpStock.setAmountOwned(sc.nextFloat());
+
+		printUI(this.printWidth, "Enter quantityOwned of" + tmpStock.getName());
+		tmpStock.setQuantityOwned(sc.nextFloat());
+
+		System.out.println(tmpStock);
+
+		return null;
+	}
+
+	private void uiLoop() {
+		Boolean uiRunning = true;
+		Boolean portfolioLoaded = false;
+
+		StockPortfolio portfolio1 = null;
+
+		int waitTime = 1000;
+
+		while (uiRunning) {
+			clearScreen();
+			System.out.println("===========================================================");
+			System.out.println("\t0: Create file\n\t1: Load file\n\t2: Save file\n\t3: Add stock");
 			System.out.println("\t4: Print portfolio\n\t5: Run prediction");
 			System.out.println("\t6: exit");
 			System.out.println("===========================================================");
@@ -209,33 +348,40 @@ public class StockCalculator2 {
 			int userInput = sc.nextInt();
 
 			switch(userInput) {
+				case 0: 
+					// System.out.println("do create file stuff");
+					// wait(waitTime);
+					portfolio1 = createFile();
+					break;
 				case 1:
 					System.out.println("do load file stuff");
-					wait(500);
+					wait(waitTime);
 					break;
 				case 2:
 					System.out.println("do save file stuff");
-					wait(500);
+					wait(waitTime);
 					break;
 				case 3:
 					System.out.println("do add stock stuff");
-					wait(500);
+					wait(waitTime);
+					portfolio1 = addStock(portfolio1);
 					break;
 				case 4:
 					System.out.println("do print portfolio stuff");
-					wait(500);
+					wait(waitTime);
 					break;
 				case 5:
 					System.out.println("do run prediction stuff");
-					wait(500);
+					wait(waitTime);
 					break;
 				case 6:
 					System.out.println("do exit stuff");
-					wait(500);
+					wait(waitTime);
 					uiRunning = false;
 					break;
 				default:
 					System.out.println("do invalid input stuff");
+					wait(waitTime);
 			}
 
 			// while loop
@@ -245,7 +391,7 @@ public class StockCalculator2 {
 	}
 
 	// Source: https://stackoverflow.com/a/32295974
-	public static void clearScreen() {  
+	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");  
 		System.out.flush();  
 	}
